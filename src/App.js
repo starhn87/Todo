830453b4c44fc0871994764ef40fa267493a44c0
starhn87 <starhn87@gmail.com>
2022-02-12 16:31:13 +0900

    컴포넌트 및 css 구성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useState } from '../node_modules/react/cjs/react.development';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
 
 function App() {
-  return <div>Todo Application</div>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어보기',
+      checked: false,
+    },
+  ]);
+
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList todos={todos} />
+    </TodoTemplate>
+  );
 }
 
 export default App;
